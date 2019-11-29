cc33ac5cc99cb614910a87e84731de18a74c80cb
Julio974-Theexahedron <jules.bour.1@gmail.com>
2019-11-29 23:38:00 +0100

    Added footers and current government summary

---
+++ script.js
@@ -0,0 +1,6 @@
+function footer() {
+	var html = '<footer>';
+    html += 'Website made by <a style="color:darkslategray" href="https://discordapp.com/channels/@me/247308028649537536">Julio974</a> &bull; See on <a style="color:darkslategray" href="https://github.com/LME2/lme2.github.io">Github</a> &bull; <a style="color:darkslategray" href="https://lme2.github.io/sitemap.html">Sitemap</a>';
+    html += '</footer>';
+	document.write(html);
+}
